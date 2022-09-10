c17bd2fb4948d831831f3580b48d2416efedeb57
creyes25 <carolina.reyes2022@gmail.com>
2022-09-10 17:49:41 -0500

    Add cached elements for all the buttons

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
+const startBtn = document.querySelector('#start')
+const feedBtn = document.querySelector('#feed-btn')
+const attentionBtn = document.querySelector('#attention-btn')
+const entertainBtn = document.querySelector('#entertain-btn')
+const restBtn = document.querySelector('#rest-btn')
 
 
 

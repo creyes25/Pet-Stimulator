5cdf364bc1f189e07b37abf8101aa05e5b593fe8
creyes25 <carolina.reyes2022@gmail.com>
2022-09-10 19:11:35 -0500

    setup increaseMoood function and init function

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+let moodProgress = 0 
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -14,28 +14,26 @@ const feedBtn = document.querySelector('#feed-btn')
 const attentionBtn = document.querySelector('#attention-btn')
 const entertainBtn = document.querySelector('#entertain-btn')
 const restBtn = document.querySelector('#rest-btn')
+const progressBar = document.querySelector('.progress-bar')
 
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-startBtn.addEventListener('click', (evt) => {
-})
-feedBtn.addEventListener('click', (evt) => {
-  
-})
-attentionBtn.addEventListener('click', (evt) => {
-  
-})
-entertainBtn.addEventListener('click', (evt) => {
-  
-})
-restBtn.addEventListener('click', (evt) => {
-  console.log(evt.target)
-
-})
-
+// startBtn.addEventListener('click', increaseMood)
+feedBtn.addEventListener('click', increaseMood)
+attentionBtn.addEventListener('click', increaseMood)
+entertainBtn.addEventListener('click', increaseMood)
+restBtn.addEventListener('click', increaseMood)
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function init() {
+
+}
+
+function increaseMood(evt) {
+  console.log(evt.target)
+}
